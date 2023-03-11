59011409c1da7debc59e72dc63d6d36752c089ef
Shubham Pagare <shubhamp2017@gmail.com>
2023-03-11 11:23:18 +0530

    Added assertion

--- Recursion/sumOfDigits.py
+++ Recursion/sumOfDigits.py
@@ -1,9 +1,9 @@
 def sumOfDigits(n):
     """This function returns sum of digits of of positive integer number."""
-    
+    assert n>=0 and int(n) == n, "Number should be postive number."
     if(n%10 == n):
         return n
     else:
         return n%10 + sumOfDigits(int(n/10))
     
-print(sumOfDigits(283))
+print(sumOfDigits(0))
